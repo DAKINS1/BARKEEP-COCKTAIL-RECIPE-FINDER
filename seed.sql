4c5d63a4f8ace447f0b1303cd124f6bc144736ac
Bryan <bryan89tran@yahoo.com>
2017-10-08 14:29:18 -0700

    data into the table

---
+++ seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO Drinks (drink_name, ingredients, url)
+    VALUES
+    ('Crane Berry Vodka','vodka 1/2,Cranberry Juice 1/2','https://assets.absolutdrinks.com/drinks/solid-background-black/soft-shadow/floor-reflection/415x655/vodka-cranberry(85).jpg'),
+    ('Malibu Breeze','Mabibu 1,Orange Juice 1/2, vodka 1, Pinapple Juice 1,Cranberry Juice .5','https://assets.absolutdrinks.com/drinks/solid-background-black/soft-shadow/floor-reflection/415x655/malibu-seabreeze(85).jpg'),
+    ('RedHead','Lime 1,Crane Berry 1/2,Crane Berry 1, Whipcream','https://assets.absolutdrinks.com/drinks/solid-background-black/soft-shadow/floor-reflection/415x655/redhead(85).jpg'),
+    ('Gentle Sea Breeze', 'Cranberry Juice 4,Grapefruit Juice 1, Lemon Wedge 1', 'https://assets.absolutdrinks.com/drinks/solid-background-black/soft-shadow/floor-reflection/415x655/gentle-sea-breeze(85).jpg'),
+    ('Absolut Raspberri Ambitious','Absolut Raspberri 1,Cranberry Juice 1,Lime Juice 2/3,Lemon-lime Soda','https://assets.absolutdrinks.com/drinks/solid-background-black/soft-shadow/floor-reflection/415x655/absolut-raspberri-ambitious(85).jpg');
